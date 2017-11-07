b510fb92bd4f024df10d5a35f2089e378f931e80
NiflheimMist <emilyehansson@hotmail.com>
2017-11-06 18:56:47 -0600

    Use the new people and students

--- GitHub Test/HelloWorld/src/HelloWorld.java
+++ GitHub Test/HelloWorld/src/HelloWorld.java
@@ -5,6 +5,11 @@
 public class HelloWorld {
 	public static void main (String args[]){
 		System.out.println("Hello there!");
-		System.out.print(".");
+		
+		Person person = new Person("Bob");
+		person.sayName();
+		
+		Student student = new Student("Jack");
+		student.sayName();
 	}
 }
