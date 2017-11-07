a8f61c74f2c897ed919046cec11b0b4f4a5319c6
EmilyEH <emilyehansson@hotmail.com>
2017-11-06 18:46:39 -0600

    Initial code

---
+++ GitHub Test/HelloWorld/src/HelloWorld.java
@@ -0,0 +1,10 @@
+/*Emily Hansson 
+ * GitHub test code 
+ * 11/6117
+ */
+public class HelloWorld {
+public static void main (String args[]){
+	System.out.println("Hello World");
+	System.out.print(".");
+}
+}
