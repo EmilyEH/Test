7a48aa69df9d5fac99118f4d89b9da1b5546a0ad
EmilyEH <emilyehansson@hotmail.com>
2017-11-06 18:49:40 -0600

    Changed phrase

--- GitHub Test/HelloWorld/src/HelloWorld.java
+++ GitHub Test/HelloWorld/src/HelloWorld.java
@@ -3,8 +3,8 @@
  * 11/6117
  */
 public class HelloWorld {
-public static void main (String args[]){
-	System.out.println("Hello World");
-	System.out.print(".");
-}
+	public static void main (String args[]){
+		System.out.println("Hello there!");
+		System.out.print(".");
+	}
 }
