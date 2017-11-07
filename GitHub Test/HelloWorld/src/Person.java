e8208eba36ae996b1c9f43b36edf2446674f3ff2
NiflheimMist <emilyehansson@hotmail.com>
2017-11-06 18:56:36 -0600

    Created people and students

---
+++ GitHub Test/HelloWorld/src/Person.java
@@ -0,0 +1,13 @@
+
+public class Person {
+	public Person(String name){
+		this.name = name;
+		
+	}
+	
+	public void sayName(){
+		System.out.println(name);
+	}
+	
+	protected String name;
+}
---
+++ GitHub Test/HelloWorld/src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student extends Person{
+
+	public Student(String name) {
+		super(name);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public void sayName(){
+		System.out.println("Student: "+name);
+	}
+
+}
